57a7548d6afc286f8a675568dd6bb71e5267a9d1
ewrf3 <q304062@163.com>
2021-07-16 19:02:11 +0800

    recommendSong

---
+++ pages/recommendSong/recommendSong.js
@@ -0,0 +1,137 @@
+// pages/recommendSong/recommendSong.js
+import request from "../../utils/request";
+
+let startY = 0; // 手指起始的坐标
+let moveY = 0; // 手指移动的坐标
+let moveDistance = 0; // 手指移动的距离
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    coverTransform: 'translateY(0)',
+    coveTransition: '',
+
+    day:'',
+    month:'',
+    recommendList: [], // 推荐列表数据
+    index: 0, // 点击音乐的下标
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // 判断用户是否登录
+    let userInfo = wx.getStorageSync('userInfo');
+    if(!userInfo){
+      wx.showToast({
+        title: '请先登录',
+        icon: 'none',
+        success: () => {
+          // 跳转至登录界面
+          wx.reLaunch({
+            url: '/pages/login/login'
+          })
+        }
+      })
+    }
+
+    //更新日期数据
+    this.setData({
+      day:new Date().getDate(),
+      month: new Date().getMonth() + 1
+    })
+
+     // 获取每日推荐的数据
+     this.getRecommendList();
+  },
+  
+  // 获取用户每日推荐数据
+  async getRecommendList(){
+    let recommendListData = await request('/recommend/songs');
+    this.setData({
+      recommendList: recommendListData.recommend
+    })
+  },
+    
+  handleTouchStart(event){
+    this.setData({
+      coveTransition: ''
+    })
+    // 获取手指起始坐标
+    startY = event.touches[0].clientY;
+  },
+  handleTouchMove(event){
+    moveY = event.touches[0].clientY;
+    moveDistance = moveY - startY;
+    
+    if(moveDistance <= 0){
+      return;
+    }
+    if(moveDistance >= 80){
+      moveDistance = 80;
+    }
+    // 动态更新coverTransform的状态值
+    this.setData({
+      coverTransform: `translateY(${moveDistance}rpx)`
+    })
+  },
+  handleTouchEnd(){
+    // 动态更新coverTransform的状态值
+    this.setData({
+      coverTransform: `translateY(0rpx)`,
+      coveTransition: 'transform 0.5s ease'
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
